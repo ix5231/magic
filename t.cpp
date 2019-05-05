a7e672ad5ba5285af3e1b1ddb9e4f3731e98ff33
Mitsuki <ix5231if@gmail.com>
2019-05-05 12:04:56 +0900

    Initial

---
+++ t.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <limits>
+
+#define seq(i, n) for (int i = 0; i < (n), i++)
+#define seq2(i, f, t) for (int i = (f); i < (n), i++)
+#define rev(i, n) for (int i = (n); i >= 0, i--)
+#define rev2(i, f, t) for (int i = (f); i >= t, i--)
+#define all(x) (x).begin(),(x).end()
+#define max(x, y) x > y ? x : y
+#define min(x, y) x < y ? x : y
+
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+
+}
+
+int main() {
+  solve(); 
+
+  return 0;
+}
