5ff63cc608fb8d6577741df0d68d6e4196215b58
Mitsuki Watanabe <ix5231if@gmail.com>
2019-06-05 18:49:46 +0900

    Fix some (stupid) mistake

--- t.cpp
+++ t.cpp
@@ -8,12 +8,12 @@
 #include <limits>
 
 #define seq(i, n) for (int i = 0; i < (n); i++)
-#define seq2(i, f, t) for (int i = (f); i < (n); i++)
+#define seq2(i, f, t) for (int i = (f); i < (t); i++)
 #define rev(i, n) for (int i = (n); i >= 0; i--)
-#define rev2(i, f, t) for (int i = (f); i >= t; i--)
+#define rev2(i, f, t) for (int i = (f); i >= (t); i--)
 #define all(x) (x).begin(),(x).end()
-#define max(x, y) x > y ? x : y
-#define min(x, y) x < y ? x : y
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
 
 using namespace std;
 
